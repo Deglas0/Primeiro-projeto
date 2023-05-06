4a142a3a114cf2c05cca2e42a6ebce0390c2643a
Douglas miguel da silva <124850234+Deglas0@users.noreply.github.com>
2023-05-06 00:13:06 -0300

    catalogo
    Add um filme ao catalogo

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
+import java.sql.SQLOutput;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("alô mundo!");
+        System.out.println("Esse é o screen match");
+        System.out.println("Filme: top gun maverick");
+
+        int AnoDeLancamento = 2022;
+        System.out.println("ano de lançamento: " + AnoDeLancamento);
+        Boolean IncluidoNoPlano = true;
+         Double NotaDoFilme = 8.1;
+//Média calculada por opnioes de coçegas;
+        Double media = (9.8 + 6.3 + 8.0)/3;
+        System.out.println(media);
+        String Sinopse;
+        Sinopse = """
+         Filme Top Gun
+         Filme de aventura com galã dos anos 80
+        Muito bom!
+        Ano de lançamento
+        """ + AnoDeLancamento;
+        System.out.println(Sinopse);
     }
+
 }
